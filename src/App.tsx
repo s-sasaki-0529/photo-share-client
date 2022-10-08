e66f759c3f86e432bfe77ac405ed351dfb735acf
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-10-08 22:07:39 +0900

    6.4.1 ユーザー認可

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import { gql } from "@apollo/client";
 import { Users } from "./components/Users";
+import { BrowserRouter } from "react-router-dom";
+import AuthorizedUser from "./components/AuthorizedUser";
 
 export const ROOT_QUERY = gql`
+  fragment userInfo on User {
+    githubLogin
+    name
+    avatar
+  }
   query allUsers {
     totalUsers
     allUsers {
-      githubLogin
-      name
-      avatar
+      ...userInfo
+    }
+    me {
+      ...userInfo
     }
   }
 `;
 
 function App() {
-  return <Users />;
+  return (
+    <BrowserRouter>
+      <AuthorizedUser />
+      <Users />;
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/AuthorizedUser.tsx
@@ -0,0 +1,43 @@
+import { gql, useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const GITHUB_AUTH_MUTATION = gql`
+  mutation githubAuth($code: String!) {
+    githubAuth(code: $code) {
+      token
+    }
+  }
+`;
+
+export const AuthorizedUser: React.FC = () => {
+  const navigate = useNavigate();
+  const [signingIn, setSigningIn] = useState(false);
+  const [requestGithubAuth] = useMutation(GITHUB_AUTH_MUTATION);
+
+  useEffect(() => {
+    if (window.location.search.match(/code=/)) {
+      setSigningIn(true);
+      const code = window.location.search.replace("?code=", "");
+      requestGithubAuth({ variables: { code } }).then((result) => {
+        const token = result.data.githubAuth.token;
+        localStorage.setItem("token", token);
+        navigate("/", { replace: true });
+        setSigningIn(false);
+      });
+    }
+  }, []);
+
+  const requestCode = () => {
+    const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID || "";
+    window.location.href = "https://github.com/login/oauth/authorize?client_id=" + clientId + "&scope=user";
+  };
+
+  return (
+    <button onClick={requestCode} disabled={signingIn}>
+      Sign In with GitHub
+    </button>
+  );
+};
+
+export default AuthorizedUser;
