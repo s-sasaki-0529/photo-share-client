3cc241148eb4e94a8aa98e9bca7ff81eca62f45f
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2022-10-08 20:29:50 +0900

    6.3.4 Mutation コンポーネント

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,3 +1,5 @@
+import { gql, useMutation } from "@apollo/client";
+import { ROOT_QUERY } from "../App";
 import UserListItem from "./UserListItem";
 
 type Props = {
@@ -9,11 +11,27 @@ type Props = {
   refetchUsers: () => void;
 };
 
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
 export const UserList: React.FC<Props> = (props) => {
+  const [addFakeUsers] = useMutation(ADD_FAKE_USERS_MUTATION, {
+    variables: { count: 1 },
+    refetchQueries: [{ query: ROOT_QUERY }],
+  });
+
   return (
     <div>
       <p>{props.count}</p>
       <button onClick={() => props.refetchUsers()}>refetchUsers</button>
+      <button onClick={() => addFakeUsers()}>addFakeUsers</button>
       <ul>
         {props.users.map((user) => (
           <UserListItem key={user.name} name={user.name} avatar={user.avatar} />
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,26 +1,12 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 import { useEffect } from "react";
 import { ROOT_QUERY } from "../App";
 import { UserList } from "./UserList";
 
 export const Users: React.FC = () => {
-  const { loading, error, data, refetch, startPolling, stopPolling } =
-    useQuery(ROOT_QUERY);
-
-  useEffect(() => {
-    startPolling(1000);
-    return () => {
-      stopPolling();
-    };
-  }, []);
+  const { loading, error, data, refetch } = useQuery(ROOT_QUERY);
 
   if (loading) return <p>Loading users...</p>;
 
-  return (
-    <UserList
-      count={data.totalUsers}
-      users={data.allUsers}
-      refetchUsers={() => refetch()}
-    />
-  );
+  return <UserList count={data.totalUsers} users={data.allUsers} refetchUsers={() => refetch()} />;
 };
